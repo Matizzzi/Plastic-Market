d5f92a99b7deca3ed7ede5e5fd4e177774f77e40
Felipee <fel.hermosilla@duocuc.cl>
2025-01-21 15:48:13 -0300

    commit inicial

--- src/pages/somos.tsx
+++ src/pages/somos.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/shared/header";
 import '../styles/globals.css';
 import CuerpoSomos from "../components/Somos/cuerpo";
+import Footer from "../components/inicio/Footer/footer";
 
 
 const Somos: React.FC = () => {
@@ -10,7 +11,8 @@ const Somos: React.FC = () => {
       
       <Header />
       <CuerpoSomos />
-    
+      <Footer />
+      
     </div>
   );
 };
